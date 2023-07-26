56f623a8f544e60658fa4cebe87c4efedaea2534
Arzuuuu <arzu22786@gmail.com>
2023-07-26 13:03:17 +0530

    first_commit

---
+++ donation_page.js
@@ -0,0 +1,7 @@
+// JavaScript to handle the toggle of the navigation links on smaller screens
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
